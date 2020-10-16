9e54fb126a8ff91547aca254b0d4fbddc2e83a2d
SukruthaM98 <msukrutha@gmail.com>
2020-10-16 23:46:29 +0530

    Checked whether employee is present or absent

--- src/program/EmpWage.java
+++ src/program/EmpWage.java
@@ -1,7 +1,19 @@
 package program;
+import utility.Utility;
+/**@author Sukrutha Manjunath
+ * Create Date   : 17th-october-2020
+ * Functionality : Calculating Employee Wage
+ */
 
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        Utility util=new Utility();
+        double attendance = util.EmpAttendance();
+        if (attendance == 1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
     }
 }
