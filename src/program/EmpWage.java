6c91c8ca7836317962248ad512f6c08ffc77c956
SukruthaM98 <msukrutha@gmail.com>
2020-10-20 09:32:38 +0530

    Refactored code using class methods and class variables

--- src/program/EmpWage.java
+++ src/program/EmpWage.java
@@ -1,9 +1,9 @@
 package program;
 import utility.Utility;
-import java.util.Scanner;
 
 /**@author Sukrutha Manjunath
  * Create Date   : 17th-october-2020
+ * Modified Date : 20th-october-2020
  * Functionality : Calculating Employee Wage
  */
 
@@ -11,7 +11,8 @@ public class EmpWage {
     public static void main(String[] args) {
         //welcome message
         System.out.println("Welcome to Employee Wage Computation Program");
-        //computing salary of employee for either 100 hours or 20 days
+        //computing salary of employee for either 100 hours or 20 days using
+        //   class methods and class variables
         Utility util=new Utility();
         int salary=util.Condition();
         System.out.println("Total Salary of the employee is " +salary);
